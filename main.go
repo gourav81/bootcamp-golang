218662c3425bb5b7cc2ac8581df5589d802b594c
gourav81 <gouravyadavme@gmail.com>
2022-07-13 17:12:51 +0530

    added a panic for db connection

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
 	if err != nil {
 		fmt.Println("Status:", err)
+		panic("Cannot connect to DB")
 	}
 	defer Config.DB.Close()
 	Config.DB.AutoMigrate(&Models.Order{}, &Models.Product{})
